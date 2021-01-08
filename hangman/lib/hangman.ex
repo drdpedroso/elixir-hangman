b7757f1860164a7c1d23b260050e3b17ef25c568
Eduardo <drdpedroso@gmail.com>
2021-01-08 08:56:34 -0300

    Add genserver

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,13 +1,17 @@
 defmodule Hangman do
 
-  alias Hangman.Game
+  alias Hangman.Server
   
-  defdelegate new_game(), to: Game
-  defdelegate tally(game), to: Game
+  def new_game() do
+    Server.start_link()
+  end
 
-  def make_move(game, guess) do
-    game = Game.make_move(game, guess)
-    { game, tally(game) }
+  def make_move(game_pid, guess) do
+    GenServer.call(game_pid, {:make_move, guess})
+  end
+
+  def tally(game_pid) do
+    GenServer.call(game_pid, { :tally })
   end
 
 end
--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -15,49 +15,49 @@ defmodule GameTest do
         game = Game.new_game()
                |> Map.put(:game_state, state)
     
-        assert ^game = Game.make_move(game, "x")
+        assert {game, _} = Game.make_move(game, "x")
     end
   end
 
   test "first ocurrence of letter is not already used" do 
     game = Game.new_game()
-    game = Game.make_move(game, "x")
+    { game, _tally } = Game.make_move(game, "x")
     assert game.game_state != :already_used
     assert "x" in game.used
   end
 
   test "first ocurrence of letter is already used" do
-      game = Game.new_game()
-    game = Game.make_move(game, "x")
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
     assert game.game_state != :already_used
     assert "x" in game.used
-    game = Game.make_move(game, "x")
+    { game, _tally } = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
 
   test "a good guess is recognized" do
-    game = Game.new_game("wibble")
-    game = Game.make_move(game, "w")
+    game= Game.new_game("wibble")
+    { game, _tally } = Game.make_move(game, "w")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
   end
 
   test "a guessed word is a won game" do
-    game = Game.new_game("wibble")
-    game = Game.make_move(game, "w")
+    game= Game.new_game("wibble")
+    { game, _tally } = Game.make_move(game, "w")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
-    game = Game.make_move(game, "i")
+    { game, _tally } = Game.make_move(game, "i")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
-    game = Game.make_move(game, "b")
+    { game, _tally } = Game.make_move(game, "b")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
-    game = Game.make_move(game, "l")
+    { game, _tally } = Game.make_move(game, "l")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
-    game = Game.make_move(game, "e")
+    { game, _tally } = Game.make_move(game, "e")
     assert game.game_state == :won
     assert game.turns_left == 7
   end
@@ -65,25 +65,25 @@ defmodule GameTest do
 
   test "bad guess is recongnized" do
       game = Game.new_game("w")
-      game = Game.make_move(game, "x")
+      { game, _tally } = Game.make_move(game, "x")
       assert game.game_state == :bad_guess
       assert game.turns_left == 6
-      game = Game.make_move(game, "y")
+      { game, _tally } = Game.make_move(game, "y")
       assert game.game_state == :bad_guess
       assert game.turns_left == 5
-      game = Game.make_move(game, "s")
+      { game, _tally } = Game.make_move(game, "s")
       assert game.game_state == :bad_guess
       assert game.turns_left == 4
-      game = Game.make_move(game, "a")
+      { game, _tally } = Game.make_move(game, "a")
       assert game.game_state == :bad_guess
       assert game.turns_left == 3
-      game = Game.make_move(game, "f")
+      { game, _tally } = Game.make_move(game, "f")
       assert game.game_state == :bad_guess
       assert game.turns_left == 2
-      game = Game.make_move(game, "l")
+      { game, _tally } = Game.make_move(game, "l")
       assert game.game_state == :bad_guess
       assert game.turns_left == 1
-      game = Game.make_move(game, "m")
+      { game, _tally } = Game.make_move(game, "m")
       assert game.game_state == :lost
   end
 
